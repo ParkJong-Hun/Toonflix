ca47c62859839ff93a4fb60d4b7030ba05d76d1a
ParkJong-Hun <thesn515@gmail.com>
2023-07-01 19:37:07 +0900

    feat: set background color black

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter!'),
-        ),
+        backgroundColor: Colors.black,
         body: Center(
           child: Text('Hello world!'),
         ),

3d6c3f3ee64eb11e4e3cb537dc5f82b72c2d6e28
ParkJong-Hun <thesn515@gmail.com>
2023-07-19 22:26:40 +0900

    feat: clean app

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,7 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: const Color(0xFFE7626C),
-        textTheme: const TextTheme(
-          headline1: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: HomeScreen(),
-    );
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
